be41b7f94b1aebb58603bebee89ba44019fc891d
R User <ruser@ip-10-164-108-61.ec2.internal>
2014-02-03 13:14:26 +0000

    Fixed multiple errors; the code is more functional. Still working on some issues.

--- import/getFLOSSmoleDataXML.R
+++ import/getFLOSSmoleDataXML.R
@@ -25,6 +25,9 @@ REPO_MONTH <- c("Dec",  "Nov",  "Nov",  "Feb",  "Sep",  "Dec",  "Dec")
 repo <- data.frame(code = REPO_CODE, year = REPO_YEAR, month = REPO_MONTH,
                    stringsAsFactors=FALSE)
 
+BZIP_EXT <- ".txt\\.bz2"
+BZIP_FNAME <- ".*\\.txt\\.bz2"
+
 
 importRepoFiles <- function(row){
   
@@ -36,16 +39,22 @@ importRepoFiles <- function(row){
                repo$year[row], "-", repo$month[row],
                collapse="", sep="")
 
-  htmlPage <- getURL(url)
+  htmlPage <- getURL(url, followlocation = TRUE)
   
-  if (FALSE) { # via HTML tables
+  if (TRUE) { # via HTML tables
     doc <- htmlParse(htmlPage)
     tables <- getNodeSet(doc, "//table")
-    filenames <- readHTMLTable(tables[[1]], skip.rows = 1)
+    filenames <- readHTMLTable(tables[[1]], skip.rows = 7,
+                               trim = TRUE,
+                               stringsAsFactors = FALSE)
+    
     filenames <- list(filenames[,2])
+    
+    filenames <- lapply(filenames,
+                        function(x) grep(BZIP_EXT, x, value = TRUE))
   }
   
-  if (TRUE) { # via XML elements
+  if (FALSE) { # via XML elements
     doc <- htmlParse(htmlPage)
     #links = getNodeSet(doc, "//table[@class='a href']")
     ##links = getNodeSet(doc, "//a/@href")
@@ -53,29 +62,41 @@ importRepoFiles <- function(row){
     ###links <- xpathSApply(doc, "//link[@rel='alternate']", xmlAttrs)
     
     #links <- xpathSApply(doc, "//a/@href")
-    links <- xpathSApply(doc, '//*[@class="href"]', xmlValue)
+    links <- xpathSApply(doc, "//*/a[@class='href']", xmlValue)
     print(doc)
     
     #links <- grepl(".txt.bz2", links)
     print(links)
     xpathSApply(doc, '//*[@class="href"]', xmlAttrs)
+
+    links <- lapply(links, function(x) grep(".txt.bz2", x, links))
   }
-  
-  #links <- lapply(links, function(x) paste(url, x, sep=""))
-  links <- lapply(links, function(x) grep(".txt.bz2", links))
   
   curlHandle <- getCurlHandle()
   #opts = curlOptions(wildcardmatch = "\\.txt.bz2$")  
   #curlSetOpt(.opts <- list(wildcardmatch = "\\.txt.bz2$"), curl = curlHandle)  
   
-  #repoFiles <- list()
-  repoFiles <- lapply(links,
-                      function(url) try(getURL(url, curl = curlHandle)))
+  # 'links' is a list of files' FULL URLs
+  links <- lapply(filenames, function(x) paste(url, x, sep="/"))
+  print(links)
   
-  for (file in repoFiles) {
-    #file <- file(url)
-    data <- read.table(bzfile(file), header=TRUE, sep=",", row.names=NULL)
-  }
+  repoFiles <- lapply(links,
+                      function(url) try(getURL(url,
+                                               curl = curlHandle,
+                                               followlocation = TRUE)))
+  print(repoFiles)
+
+  # Tried to call URLdecode(links), but still produces two errors:
+  # "Error in curlMultiPerform(multiHandle) : embedded nul in string:"
+  # "Error in bzfile(links) : invalid 'description' argument"
+  data <- lapply(links,
+                 function(url) try(read.table(bzfile(links),
+                                              header=TRUE, sep=",", row.names=NULL)))
+  
+  #for (file in repoFiles) {
+    #data <- read.table(bzfile(file, open = "r"),
+    #                   header=TRUE, sep=",", row.names=NULL)
+  #}
 }
 
 
