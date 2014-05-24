e8b19b53ed6faa1389bb12bd9281a8134ea55936
Aleksandr Blekh <blekh@nova.edu>
2014-05-24 09:32:07 -0400

    Redefine ggplot2::qplot() to print the graphics object in order to actually produce output in non-interactive mode.

---
+++ utils/graphics.R
@@ -0,0 +1,14 @@
+#
+# Redefine ggplot2::qplot() to print the graphics object
+# in order to actually produce output in non-interactive mode
+#
+# For details, see:
+#
+# 1. http://stackoverflow.com/questions/6675066/ggplots-qplot-does-not-execute-on-sourcing
+# 2. http://cran.r-project.org/doc/FAQ/R-FAQ.html#Why-do-lattice_002ftrellis-graphics-not-work_003f)
+#
+
+qplot <- function (x, y = NULL, z = NULL, ...) {
+  p <- ggplot2::qplot (x = x, y = y, z = z, ...)
+  print (p)
+}
