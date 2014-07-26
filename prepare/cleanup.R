60cbdbc029af3f76787148bbd7d9faa5cba2ec49
Aleksandr Blekh <blekh@nova.edu>
2014-07-25 21:06:30 -1100

    Started working on data cleanup. Installed R packages "editrules" and "deducorrect". Tested for error-free loading of these packages.

---
+++ prepare/cleanup.R
@@ -0,0 +1,5 @@
+if (!suppressMessages(require(editrules))) install.packages('editrules')
+if (!suppressMessages(require(deducorrect))) install.packages('deducorrect')
+
+library(editrules)
+library(deducorrect)
