95afad1ec4cfb0d6042ab7b94bd2a5bac153f0b6
Aleksandr Blekh <blekh@nova.edu>
2014-08-11 16:43:17 -0400

    Fixed 'rds2rda()' utility to ensure correct storing of R objects in the RDA file.

--- utils/rds2rda.R
+++ utils/rds2rda.R
@@ -8,16 +8,18 @@ rds2rda <- function(dataDir = ".", archiveFile = "mergeTestData.rda") {
   workDir <- getwd() # save working directory
   setwd(dataDir) # set new working directory
   
-  objects <- list()
+  objects <- c()
   
   for (file in dir(pattern='\\.rds$')) {
     nameSplit <- strsplit(file, "\\.")
     objName <- nameSplit[[1]][1]
     assign(objName, readRDS(file))
-    objects <- c(objects, get(objName))
+    #objects <- c(objects, get(objName))
+    objects <- c(objects, objName)
   }
-  #save(list = objects, file = archiveFile)
-  save(objects, file = archiveFile)
+
+  #save(objects, file = archiveFile)
+  save(list = objects, file = archiveFile)
   
   setwd(workDir) # restore the original working directory
 }
