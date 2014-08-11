eeb3a9142fcb8298b5647da61694a4eddd2c3d6f
Aleksandr Blekh <blekh@nova.edu>
2014-08-10 23:03:10 -1100

    Created a function (utility) that converts indicator-named RDS files into a single RDA file with correspondingly named objects.

---
+++ utils/rds2rda.R
@@ -0,0 +1,23 @@
+# Function (utility) that converts indicator-named RDS files
+# into a single RDA file with correspondingly named objects.
+
+rm(list=ls(all=TRUE)) # Clear R environment from previous sessions
+
+rds2rda <- function(dataDir = ".", archiveFile = "mergeTestData.rda") {
+  
+  workDir <- getwd() # save working directory
+  setwd(dataDir) # set new working directory
+  
+  objects <- list()
+  
+  for (file in dir(pattern='\\.rds$')) {
+    nameSplit <- strsplit(file, "\\.")
+    objName <- nameSplit[[1]][1]
+    assign(objName, readRDS(file))
+    objects <- c(objects, get(objName))
+  }
+  #save(list = objects, file = archiveFile)
+  save(objects, file = archiveFile)
+  
+  setwd(workDir) # restore the original working directory
+}
