b9b1f04a163d45b5e561374755a9aaa65c8a0213
Aleksandr Blekh <blekh@nova.edu>
2014-08-13 23:45:32 -1100

    Refactored merge functionality into functions. Introduced "method" merge parameter with "plyr" as default, currently.

--- prepare/merge.R
+++ prepare/merge.R
@@ -1,16 +1,7 @@
+# Module responsible for merging already transformed data
+# into a single data frame for further analysis
+
 SRDA_DIR <- "~/diss-floss/data/transform/SourceForge"
-
-# To enable desired merge option, uncomment corresponding line
-
-#MERGE_OPTION <- "lapply_merge"
-#MERGE_OPTION <- "lapply_merge2" # advice by Alexey G.
-#MERGE_OPTION <- "reduce_merge"
-#MERGE_OPTION <- "reduce_merge2"
-#MERGE_OPTION <- "reshape"
-MERGE_OPTION <- "plyr"
-#MERGE_OPTION <- "dplyr"
-#MERGE_OPTION <- "data.table"
-#MERGE_OPTION <- "data.table2"
 
 
 loadData <- function (dataFile) {
@@ -42,121 +33,145 @@ loadDataSets <- function (dataDir) {
 }
 
 
-# load the datasets of transformed data
+mergeDataSets <- function (datasets, method = "plyr") {
+
+  flossData <<- data.frame()
+  
+  lapplyMerge <- function (dataSets) {
+    
+    flossData <<- data.frame(dataSets[[1]][1])
+    
+    # merge all loaded datasets by common column ("Project ID")
+    silent <- lapply(seq(2, length(dataSets)),
+                     function(i) {merge(flossData, dataSets[[1]][i],
+                                        by = "Project ID",
+                                        all = TRUE)})
+  }
+  
+  
+  lapplyMerge2 <- function (dataSets) {
+    
+    pids <- which(sapply(dataSets,
+                         FUN=function(x) {'Project ID' %in% names(x)}))
+    
+    flossData <<- dataSets[[pids[1]]]
+    
+    for (id in pids[2:length(pids)]) {
+      flossData <- merge(flossData, dataSets[[id]],
+                         by='Project ID', all = TRUE)
+    }
+  }
+  
+  
+  reduceMerge <- function (dataSets) {
+    
+    flossData <<- Reduce(function(...) 
+      merge(..., by.x = "row.names", by.y = "Project ID", all = TRUE),
+      dataSets)
+  }
+  
+  
+  # http://r.789695.n4.nabble.com/merge-multiple-data-frames-tt4331089.html#a4333772
+  reduceMerge <- function (dataSets) {
+    
+    mergeAll <- function(..., by = "Project ID", all = TRUE) {
+      dotArgs <- list(...)
+      dotNames <- lapply(dotArgs, names)
+      repNames <- Reduce(intersect, dotNames)
+      repNames <- repNames[repNames != by]
+      for(i in seq_along(dotArgs)){
+        wn <- which( (names(dotArgs[[i]]) %in% repNames) &
+                       (names(dotArgs[[i]]) != by))
+        names(dotArgs[[i]])[wn] <- paste(names(dotArgs[[i]])[wn],
+                                         names(dotArgs)[[i]], sep = ".")
+      }
+      Reduce(function(x, y) merge(x, y, by = by, all = all), dotArgs)
+    }
+    
+    flossData <<- mergeAll(dataSets)
+  }
+  
+  
+  reshapeMerge <- function (dataSets) {
+    
+    if (!suppressMessages(require(reshape))) install.packages('reshape')
+    library(reshape)
+    flossData <<- reshape::merge_all(dataSets)
+  }
+  
+  
+  plyrMerge <- function (dataSets) {
+    
+    if (!suppressMessages(require(plyr))) install.packages('plyr')
+    library(plyr)
+    
+    flossData <<- dataSets[[1]]
+    for (i in seq.int(2, length(dataSets), 1)) {
+      flossData <<- plyr::join(flossData, dataSets[[i]],
+                               by = 'Project ID',
+                               type = 'left', match = 'first')
+    }
+  }
+  
+  
+  dplyrMerge <- function (dataSets) {
+    
+    if (!suppressMessages(require(dplyr))) install.packages('dplyr')
+    library(dplyr)
+    
+    flossData <<- dataSets[[1]][1]
+    flossData <<- lapply(dataSets[[1]][-1],
+                         function(x) {dplyr::left_join(x, flossData)})
+  }
+  
+  
+  dataTableMerge <- function (dataSets) {
+    
+    if (!suppressMessages(require(data.table))) 
+      install.packages('data.table')
+    library(data.table)
+    
+    flossData <<- data.table(dataSets[[1]], key="Project ID")
+    
+    for (id in 2:length(dataSets)) {
+      flossData <<- merge(flossData, data.table(dataSets[[id]]),
+                         by='Project ID') # , all = TRUE
+    }
+  }
+  
+  
+  # http://stackoverflow.com/a/17458887/2872891
+  dataTableMerge2 <- function (dataSets) {
+    
+    if (!suppressMessages(require(data.table))) 
+      install.packages('data.table')
+    library(data.table)
+    
+    DT <- data.table(dataSets[[1]], key="Project ID")
+    flossData <<- lapply(dataSets[[1]][-1], function(x) DT[.(x)])
+  }
+
+  
+  switch(method,
+         "lapply"      = lapplyMerge (datasets),
+         "lapply2"     = lapplyMerge2 (datasets),
+         "reduce"      = reduceMerge (datasets),
+         "reduce2"     = reduceMerge2 (datasets),
+         "reshape"     = reshapeMerge (datasets),
+         "plyr"        = plyrMerge (datasets),
+         "dplyr"       = dplyrMerge (datasets),
+         "data.table"  = dataTableMerge (datasets),
+         "data.table2" = dataTableMerge2 (datasets)
+  )
+  
+  return (flossData)
+}
+
+
+# load datasets of transformed data
 dataSets <- loadDataSets(SRDA_DIR)
 
+# merge loaded datasets
+flossData <- mergeDataSets(dataSets) # method "plyr" is default
 
-if (MERGE_OPTION == "lapply_merge") { # Option 1
-  
-  flossData <- data.frame(dataSets[[1]][1])
-  
-  # merge all loaded datasets by common column ("Project ID")
-  silent <- lapply(seq(2, length(dataSets)),
-                   function(i) {merge(flossData, dataSets[[1]][i],
-                                      by = "Project ID",
-                                      all = TRUE)})
-}
-
-
-if (MERGE_OPTION == "lapply_merge2") { # Option 1
-  
-  pids <- which(sapply(dataSets,
-                       FUN=function(x) {'Project ID' %in% names(x)}))
-  
-  flossData <- dataSets[[pids[1]]]
-  
-  for (id in pids[2:length(pids)]) {
-    flossData <- merge(flossData, dataSets[[id]],
-                       by='Project ID', all = TRUE)
-  }
-}
-
-
-if (MERGE_OPTION == "reduce_merge") { # Option 2
-  
-  flossData <- Reduce(function(...) 
-    merge(..., by.x = "row.names", by.y = "Project ID", all = TRUE),
-    dataSets)
-}
-
-
-# http://r.789695.n4.nabble.com/merge-multiple-data-frames-tt4331089.html#a4333772
-if (MERGE_OPTION == "reduce_merge2") { # Option 2
-  
-  mergeAll <- function(..., by = "Project ID", all = TRUE) {
-    dotArgs <- list(...)
-    dotNames <- lapply(dotArgs, names)
-    repNames <- Reduce(intersect, dotNames)
-    repNames <- repNames[repNames != by]
-    for(i in seq_along(dotArgs)){
-      wn <- which( (names(dotArgs[[i]]) %in% repNames) &
-                     (names(dotArgs[[i]]) != by))
-      names(dotArgs[[i]])[wn] <- paste(names(dotArgs[[i]])[wn],
-                                       names(dotArgs)[[i]], sep = ".")
-    }
-    Reduce(function(x, y) merge(x, y, by = by, all = all), dotArgs)
-  }
-  
-  flossData <- mergeAll(dataSets)
-}
-
-
-if (MERGE_OPTION == "reshape") { # Option 3
-  
-  if (!suppressMessages(require(reshape))) install.packages('reshape')
-  library(reshape)
-  flossData <- reshape::merge_all(dataSets)
-}
-
-
-if (MERGE_OPTION == "plyr") { # Option 4
-  
-  if (!suppressMessages(require(plyr))) install.packages('plyr')
-  library(plyr)
-  
-  flossData <- dataSets[[1]]
-  for (i in seq.int(2, length(dataSets), 1)) {
-    flossData <- plyr::join(flossData, dataSets[[i]],
-                            by = 'Project ID',
-                            type = 'left', match = 'first')
-  }
-}
-
-
-if (MERGE_OPTION == "dplyr") { # Option 5
-  
-  if (!suppressMessages(require(dplyr))) install.packages('dplyr')
-  library(dplyr)
-  
-  flossData <- dataSets[[1]][1]
-  flossData <- lapply(dataSets[[1]][-1],
-                      function(x) {dplyr::left_join(x, flossData)})
-}
-
-
-if (MERGE_OPTION == "data.table") { # Option 6
-  
-  if (!suppressMessages(require(data.table))) 
-    install.packages('data.table')
-  library(data.table)
-  
-  flossData <- data.table(dataSets[[1]], key="Project ID")
-  
-  for (id in 2:length(dataSets)) {
-    flossData <- merge(flossData, data.table(dataSets[[id]]),
-                       by='Project ID') # , all = TRUE
-  }
-}
-
-
-# http://stackoverflow.com/a/17458887/2872891
-if (MERGE_OPTION == "data.table2") { # Option 6
-  
-  if (!suppressMessages(require(data.table))) 
-    install.packages('data.table')
-  library(data.table)
-  
-  DT <- data.table(dataSets[[1]], key="Project ID")
-  flossData <- lapply(dataSets[[1]][-1], function(x) DT[.(x)])
-}
-
+str(flossData)
