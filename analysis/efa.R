2d11e8e86438278f167dc413af16c63efecb8920
Aleksandr Blekh <blekh@nova.edu>
2014-08-14 23:50:08 -1100

    Initial revision of EFA code.

---
+++ analysis/efa.R
@@ -0,0 +1,15 @@
+# Start session with a clean R environment
+rm(list = ls(all.names = TRUE))
+
+if (!suppressMessages(require(psych))) install.packages('psych')
+library(psych)
+
+# load data
+
+# determine number of factors to extract
+
+# parallel analysis
+fa.parallel(WASIWRIT.cor, n.obs=152, fm="pa")
+
+# Velicer’s minimum average partial (MAP)
+VSS(WASIWRIT.cor, n.obs=152)
