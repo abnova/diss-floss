5403b0758ba7a4a82cb8968878c8dd9242c8be59
Aleksandr Blekh <blekh@nova.edu>
2014-09-02 20:35:12 -0400

    Investigation for mixture distributions code.

--- sandbox/reMixedDist3_LongFmt.R
+++ sandbox/reMixedDist3_LongFmt.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(scales)
 library(RColorBrewer)
+library(reshape)
 library(mixtools)
 
 NUM_COMPONENTS <- 2
@@ -25,34 +26,33 @@ calc.components <- function(x, mix, comp.number) {
     dnorm(x, mean = mix$mu[comp.number], sd = mix$sigma[comp.number])
 }
 
+# create a data frame with columns consisting of
+# calculated values for each component distribution
+my.df <- data.frame(comp1 = calc.components(myData, mix.info, 1),
+                    comp2 = calc.components(myData, mix.info, 2),
+                    data = myData)
+
+# convert to long format
+my.df.long <- melt(my.df, id.vars = "data")
+
 # we could select needed number of colors randomly:
 #DISTRIB_COLORS <- sample(colors(), numComponents)
 
 # or, better, use a palette with more color differentiation:
 DISTRIB_COLORS <- brewer.pal(numComponents, "Set1")
 
-distComps <- lapply(seq(numComponents), function(i)
-  stat_function(fun = calc.components,
-                arg = list(mix = mix.info, comp.number = i),
-                geom = "line", # use alpha=.5 for "polygon"
-                size = 1,
-                color = "red")) # DISTRIB_COLORS[i]
-
-g <- ggplot(data.frame(x = myData)) +
-  scale_fill_continuous("Count", low="#56B1F7", high="#132B43") + 
+g <- ggplot(my.df, aes(x = myData)) +
+  geom_histogram(aes(x = myData, fill = ..density..), # ..count..
+                 binwidth = 0.01) +
+  #stat_ecdf() +
+  geom_line(data = my.df.long,
+            aes(x = data, y = value, color = variable)) +
   scale_x_log10("Diamond Price [log10]",
                 breaks = trans_breaks("log10", function(x) 10^x),
                 labels = prettyNum) +
   scale_y_continuous("Count") +
-  geom_histogram(aes(x = myData, fill = ..count..), # ..count..
-                 binwidth = 0.01) +
-  #distComps
-  stat_function(fun = calc.components,
-                arg = list(mix = mix.info, comp.number = 1),
-                geom = "line", # use alpha=.5 for "polygon"
-                size = 1,
-                color = DISTRIB_COLORS[1])
-
+  scale_fill_continuous("Count", low="#56B1F7", high="#132B43")
+  #theme(legend.position = 'top', legend.direction = 'vertical')
 print(g)
 
 #print(g + distComps)
