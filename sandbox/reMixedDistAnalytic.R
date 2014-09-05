ade48867a83c563c961f512a759c9b41781c99c1
Aleksandr Blekh <blekh@nova.edu>
2014-09-04 20:09:47 -0400

    Fixed problem with incorrectly using 'mclust' package. Added several other methods (some are currently disabled).

--- sandbox/reMixedDistAnalytic.R
+++ sandbox/reMixedDistAnalytic.R
@@ -18,6 +18,7 @@ library(MASS)
 library(mixtools)
 library(ddst)
 library(rebmix)
+library(fpc)
 
 NUM_COMPONENTS <- 2
 NUM_ITERATIONS <- 100
@@ -37,11 +38,19 @@ myData <- log10(myData)
 # density estimation, based on finite normal mixture modeling.
 # Using 'mclust' package (http://www.jstatsoft.org/v18/i06/paper;
 # http://www.stat.washington.edu/research/reports/2012/tr597.pdf).
-mc <- Mclust(myData)
-print(summary(mc))
+if (FALSE) {
+  mc <- Mclust(myData)
+  print(summary(mc, parameters = TRUE))
+}
 
-bestModel <- mclustModel(myData, mc)
-print(summary(bestModel))
+if (TRUE) {
+  mc <- mclustBIC(myData)
+  print(summary(mc, myData, parameters = TRUE))
+  plot(mc)
+  
+  bestModel <- mclustModel(myData, mc)
+  print(summary(bestModel, myData))
+}
 
 # using 'mixtools' package
 #mix.sel <- multmixmodel.sel(myData, comps = 1:4, epsilon = 0.01)
@@ -49,9 +58,9 @@ print(summary(bestModel))
 
 # determine number of components in mixture distribution
 # by hypothesis testing via parametric bootstrap
-mix.boot <- boot.comp(myData, max.comp = 10, mix.type = "normalmix",
-                      maxit = 400, epsilon = 0.01)
-print(summary(mix.boot))
+#mix.boot <- boot.comp(myData, max.comp = 10, mix.type = "normalmix",
+#                      maxit = 400, epsilon = 0.01)
+#print(summary(mix.boot))
 
 # extract 'k' components from mixed distribution 'data'
 mix <- normalmixEM(myData, k = NUM_COMPONENTS,
@@ -75,7 +84,6 @@ pmlnorm <- function(x, meanlog, sdlog, pmix) {
 ks.info <- ks.test(log(myData), pmlnorm,
                    meanlog=mix$mu, sdlog=mix$sigma, pmix=mix$lambda)
 print(ks.info)
-
 
 # Use parametric bootstraping (to partially overcome bias)
 
@@ -108,6 +116,54 @@ if (FALSE) {
 }
 
 
+# A bootstrap version of K-S test, based on Monte-Carlo approach
+#
+# Fits an observed distribution with respect to a lognormal model
+# and computes p value, using method described in:
+# A. Clauset, C. R. Shalizi, M. E. J. Newman. Power-law distributions
+# in empirical data. SIAM Review 51, 661-703 (2009)
+# INPUT: 
+#   d: the observed distribution to fit
+#   limit: the number of synthetic data sets to generate
+# OUTPUT
+#   meanlog: the meanlog (mu) parameter
+#   sdlog: the sdlog (sigma) parameter
+#   stat: the KS statistic
+#   p: the percentage of time the observed distribution has a KS statistic lower than
+#      or equal to the synthetic distribution
+#   KSp: the KS p-value
+lognormalKS <- function (d, limit = 2500) {
+  
+  # load MASS package to use fitdistr
+  # mle = fitdistr(d, "lognormal")
+  # meanlog = mle$estimate["meanlog"]
+  # sdlog = mle$estimate["sdlog"]
+  
+  # MLE for lognormal distribution
+  meanlog <- mean(log(d))
+  sdlog <- sd(log(d))
+  
+  # compute KS statistic
+  t <- ks.test(d, "plnorm", meanlog = meanlog, sdlog = sdlog)
+  
+  # compute p-value
+  count = 0
+  for (i in 1:limit) {
+    syn <- rlnorm(length(d), meanlog = meanlog, sdlog = sdlog)
+    meanlog2 <- mean(log(syn))
+    sdlog2 <- sd(log(syn))
+    t2 <- ks.test(syn, "plnorm", meanlog = meanlog2, sdlog = sdlog2)
+    if (t2$stat >= t$stat) {count = count + 1}
+  }
+  
+  return (list(meanlog = meanlog, sdlog = sdlog,
+                 stat = t$stat, p = count/limit, KSp = t$p))
+}
+
+ks.logn <- lognormalKS(myData, limit = 100)
+print(ks.logn$KSp)
+
+
 # Anderson-Darling test
 
 
@@ -117,21 +173,33 @@ print(ddst.info)
 
 
 # REBMIX
-dist.est <- REBMIX(Dataset = list(data.frame(myData)),
-                   Preprocessing = "histogram",
-                   cmax = 8, Criterion = c("AIC", "BIC"),
-                   Variables = "continuous",
-                   pdf = "gamma", K = 30:80)
-summary(dist.est)
 
-plot(dist.est, pos = 1, what = c("den", "dis"), ncol = 2, npts = 1000)
-
-coef(dist.est)
-numComps <- as.numeric(dist.est$summary$c)
-summary(numComps)
+if (FALSE) {
+  dist.est <- REBMIX(Dataset = list(data.frame(myData)),
+                     Preprocessing = "histogram",
+                     cmax = 8, Criterion = c("AIC", "BIC"),
+                     Variables = "continuous",
+                     pdf = "gamma", K = 30:80)
+  summary(dist.est)
+  
+  plot(dist.est, pos = 1, what = c("den", "dis"), ncol = 2, npts = 1000)
+  
+  coef(dist.est)
+  numComps <- as.numeric(dist.est$summary$c)
+  summary(numComps)
+}
 
 # parametric bootstraping to determine number of components
-dist.boot <- boot.REBMIX(x = dist.est, pos = 1,
-                         Bootstrap = "p", B = 100,
-                         n = NULL, replace = TRUE, prob = NULL)
-summary(dist.boot)
+if (FALSE) {
+  dist.boot <- boot.REBMIX(x = dist.est, pos = 1,
+                           Bootstrap = "p", B = 100,
+                           n = NULL, replace = TRUE, prob = NULL)
+  summary(dist.boot)
+}
+
+
+# density-based clustering (DBSCAN), using 'fpc' package
+if (FALSE) {
+  dense <- dbscan(myData, eps=0.5, MinPts=5)
+  plot(dense, myData)
+}
