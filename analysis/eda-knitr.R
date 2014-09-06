cdf6b17d9ed817ed91d572ddc634f920c9a22154
Aleksandr Blekh <blekh@nova.edu>
2014-09-05 23:03:53 -1100

    Updated EDA report code with the latest EDA module code. Set knitr's working directory, using 'root.dir' option (doesn't seem to work - investigating the issue).

--- analysis/eda-knitr.R
+++ analysis/eda-knitr.R
@@ -6,25 +6,45 @@ rm(list = ls(all.names = TRUE))
 if (!suppressMessages(require(RCurl))) install.packages('RCurl')
 if (!suppressMessages(require(stringr))) install.packages('stringr')
 if (!suppressMessages(require(ggplot2))) install.packages('ggplot2')
+if (!suppressMessages(require(scales))) install.packages('scales')
+if (!suppressMessages(require(RColorBrewer)))
+  install.packages('RColorBrewer')
 if (!suppressMessages(require(gridExtra))) install.packages('gridExtra')
-if (!suppressMessages(require(knitr))) install.packages('knitr')
+if (!suppressMessages(require(psych))) install.packages('psych')
+if (!suppressMessages(require(fitdistrplus))) 
+  install.packages('fitdistrplus')
+if (!suppressMessages(require(mixtools))) install.packages('mixtools')
+if (!suppressMessages(require(rebmix))) install.packages('rebmix')
 
 library(RCurl)
 library(stringr)
 library(ggplot2)
+library(scales)
+library(RColorBrewer)
 library(gridExtra)
-library(knitr)
+library(psych)
+library(fitdistrplus)
+library(mixtools)
+library(rebmix)
 
 ## @knitr PrepareEDA
 PRJ_HOME <- getwd()
 
-source(file.path(PRJ_HOME, "../utils/factors.R"))
-source(file.path(PRJ_HOME, "../utils/qq.R"))
+source(file.path(PRJ_HOME, "utils/factors.R"))
+source(file.path(PRJ_HOME, "utils/qq.R"))
+source(file.path(PRJ_HOME, "utils/data.R"))
+source(file.path(PRJ_HOME, "utils/mixedDist.R"))
 
-TRANSFORM_DIR <- file.path(PRJ_HOME, "../data/transform")
+READY4EDA_DIR  <- file.path(PRJ_HOME, "data/ready4eda")
+READY4EDA_FILE <- "flossData" # default
 RDS_EXT <- ".rds"
 
-EDA_RESULTS_DIR <- file.path(PRJ_HOME, "../results/eda")
+# temporary, until implementing EDA for single data file
+TRANSFORM_DIR <- file.path(PRJ_HOME, "data/transformed")
+
+EDA_RESULTS_DIR <- file.path(PRJ_HOME, "results/eda")
+
+DIST_FIT_COLOR <- "green" # ggplot2 line color for distrib. fitting
 
 DEBUG <- TRUE # TODO: retrieve debug flag via CL arguments
 
@@ -38,12 +58,7 @@ uniDescriptiveEDA <- function (df, var, colName, extraFun) {
   
   data <- df[[colName]]
   
-  if (is.numeric(data)) {
-    message("\nDecriptive statistics for '", colName, "':\n")
-    print(summary(data))
-  }
-  
-  if (is.factor(data)) {
+  if (is.numeric(data) || is.factor(data)) {
     message("\nDecriptive statistics for '", colName, "':\n")
     print(summary(data))
   }
@@ -79,9 +94,130 @@ uniVisualEDA <- function (df, var, colName, extraFun) {
 }
 
 
+fitDistParam <- function (df, var, colName, extraFun) {
+  
+  data <- df[[colName]]
+  df <- na.omit(df)
+  
+  message("\nParametric distribution fitting for '", colName, "':\n")
+  
+  # convert factors to integers
+  if (is.factor(data)) data <- as.integer(data)
+  
+  if (FALSE) {
+    dataDist <- fitdist(data, "gamma")
+    dataBoot <- bootdist(dataDist, niter=51) #default niter=1001
+    print(dataBoot)
+    #plot(dataBoot)
+    print(summary(dataBoot))
+    print(quantile(dataBoot))
+    message("")
+  }
+  
+  if (is.numeric(data) || is.factor(data)) {
+    fitgmme <- fitdist(data, "gamma", method="mme")
+    print(summary(fitgmme))
+    message("")
+  }
+  
+  if (is.numeric(data) || is.factor(data)) {
+    ##fitg <- fitdist(data, "gamma")
+    ##print(summary(fitg))
+    #plot(fitg)
+    #plot(fitg, demp = TRUE)
+    #plot(fitg, histo = FALSE, demp = TRUE)
+    ##cdfcomp(fitg, addlegend=FALSE)
+    ##denscomp(fitg, addlegend=FALSE)
+    ##ppcomp(fitg, addlegend=FALSE)
+    ##qqcomp(fitg, addlegend=FALSE)
+    ##message("")
+  }
+}
+
+
+fitDistNonParam <- function (df, var, colName, extraFun) {
+  
+  if (colName == "Development Team Size") return()
+  if (colName == "Project Maturity") return()
+  if (colName == "Project License") return()
+  
+  df <- df
+  data <- df[[colName]]
+  data <- na.omit(data)
+  
+  message("\nNon-parametric distribution fitting for '", colName, "':\n")
+  
+  # convert factors to integers
+  if (is.factor(data)) data <- as.integer(data)
+  
+  # perform non-parametric mixture distribution fitting
+  if (is.numeric(data)) {
+    
+    num.components <- 3 # can determine automatically?
+    
+    mixDistInfo <- fitMixDist(data, num.components)
+    
+    #data <- data.frame(x = data)
+    g <- plotMixedDist(data, mixDistInfo, num.components,
+                       colName)
+    
+    if (.Platform$GUI == "RStudio") {print(g)}
+    
+    #TODO: consider moving to main
+    edaFile <- str_replace_all(string=colName, pattern=" ", repl="")
+    edaFile <- file.path(EDA_RESULTS_DIR, paste0(edaFile, "-DistFitMix", ".svg"))
+    suppressMessages(ggsave(file=edaFile, plot=g, width=8.5, height=11))
+    
+    allPlots <<- c(allPlots, list(g))
+    message("")
+  }
+  
+}
+
+
+fitDistREBMIX <- function (df, var, colName, extraFun) {
+  
+  data <- df[[colName]]
+  df <- na.omit(df)
+  
+  # convert factors to integers
+  if (is.factor(data)) data <- as.integer(data)
+  
+  # perform parametric mixture distribution fitting
+  if (is.numeric(data)) {
+    set.seed(123)
+    
+    message("\nREBMIX: Parametric distribution fitting for '", colName, "':\n")
+    
+    boot.param <- boot.REBMIX(x = list(data), pos = 1, Bootstrap = "p",
+                              B = 100, n = NULL, replace = TRUE, prob = NULL)
+    summary(boot.param)
+    
+    message("\nREBMIX: Non-parametric distribution fitting for '", colName, "':\n")
+    
+    boot.nonparam <- boot.REBMIX(x = list(data), pos = 1, Bootstrap = "n",
+                                 B = 100, n = NULL, replace = TRUE, prob = NULL)
+    summary(boot.nonparam)
+    
+    message("\nREBMIX: Poisson distribution fitting for '", colName, "':\n")
+    
+    poissonest <- REBMIX(Dataset = list(data), Preprocessing = "histogram",
+                         cmax = 6, Criterion = "MDL5",
+                         Variables = rep("discrete", 2),
+                         pdf = rep("Poisson", 2), K = 1)
+    c <- as.numeric(poissonest$summary$c)
+    summary(c)
+    message("")
+  }
+}
+
+
 multiDescriptiveEDA <- function (df, var, colNames, extraFun) {
   
   message("\nDecriptive statistics for '", colNames, "':\n")
+  
+  # suppress "NAs introduced by coercion" warnings
+  suppressWarnings(describe(flossData))
 }
 
 
@@ -93,8 +229,7 @@ multiVisualEDA <- function (df, var, colName, extraFun) {
 performEDA <- function (dataSource, analysis,
                         indicator, colName, extraFun) {
   
-  fileDigest <- base64(indicator)
-  fileName <- paste0(fileDigest, RDS_EXT)
+  fileName <- paste0(indicator, RDS_EXT)
   rdataFile <- file.path(TRANSFORM_DIR, dataSource, fileName)
   if (file.exists(rdataFile)) {
     data <- readRDS(rdataFile)
@@ -108,12 +243,15 @@ performEDA <- function (dataSource, analysis,
     
     uniDescriptiveEDA(data, indicator, colName, extraFun)
     uniVisualEDA(data, indicator, colName, extraFun)
+    #fitDistParam(data, indicator, colName, extraFun)
+    fitDistNonParam(data, indicator, colName, extraFun)
+    #fitDistREBMIX(data, indicator, colName, extraFun)
     
   } else if (identical(analysis, "multivariate")) {
     
     colNames <- names(data)
     colNames <- colNames[-1] # delete Project ID
-    #multiDescriptiveEDA(data, indicator, colNames, extraFun)
+    multiDescriptiveEDA(data, indicator, colNames, extraFun)
     multiVisualEDA(data, indicator, colNames, extraFun)
     
   } else {
@@ -129,10 +267,11 @@ performEDA <- function (dataSource, analysis,
 
 
 # Plot distribution of a continuous variable "colName"
-plotHistogram <- function (df, colName) {
+plotHistogram <- function (df, colName, print = TRUE) {
   
   df <- df
   df$var <- df[[colName]]
+  df <- na.omit(df)
   
   title <- paste("Projects distribution across", colName, "range")
   xLabel <- colName
@@ -140,15 +279,23 @@ plotHistogram <- function (df, colName) {
   if (identical(colName, "Project Age"))
     xLabel <- paste(colName, "(months)")
   
-  g <- ggplot(data = df, aes(x=var)) +
-    scale_fill_continuous("Number of\nprojects") + 
-    scale_x_continuous(xLabel) +
+  g <- ggplot(df, aes(x=var)) +
+    scale_fill_continuous("Number of\nprojects",
+                          low="#56B1F7", high="#132B43") + 
+    #scale_x_continuous(xLabel) +
     #scale_y_continuous("Number of projects") +
-    #scale_x_log10(xLabel) +
-    scale_y_log10("Number of projects") +
+    scale_x_log10(xLabel) +
+    scale_y_log10("Number of projects",
+                  breaks = trans_breaks("log10", function(x) 10^x),
+                  #labels = trans_format("log10", math_format(10^.x))
+                  labels = prettyNum) +
     ggtitle(label=title)
   
-  g <- g + geom_histogram(aes(fill = ..count..), binwidth = 1,
+  breaks <- pretty(range(df$var), n = nclass.FD(df$var), min.n = 1)
+  bwidth <- breaks[2] - breaks[1]
+  
+  g <- g + geom_histogram(aes(fill = ..count..),
+                          binwidth = 0.01, #bwidth, #0.1
                           position = "identity")
   
   # Overlay with transparent density plot
@@ -156,8 +303,14 @@ plotHistogram <- function (df, colName) {
   
   # Ignore NA values for mean
   g <- g + geom_vline(aes(xintercept=mean(var, na.rm=T)),
-                      color="red", size=1)
-  print(g)
+                      linetype = "longdash", color="red")
+  
+  if (.Platform$GUI == "RStudio") {print(g)}
+  
+  #TODO: consider moving to main
+  edaFile <- str_replace_all(string=colName, pattern=" ", repl="")
+  edaFile <- file.path(EDA_RESULTS_DIR, paste0(edaFile, ".svg"))
+  suppressMessages(ggsave(file=edaFile, plot=g, width=8.5, height=11))
   
   return (g)
 }
@@ -169,6 +322,7 @@ plotDensity <- function (df, colName) {
   df <- df
   df$var <- df[[colName]]
   df$category <- factor(df[[colName]])
+  df <- na.omit(df)
   
   title <- paste("Projects distribution across", colName,
                  "range (by category)")
@@ -178,8 +332,12 @@ plotDensity <- function (df, colName) {
   #    geom_bar(aes(y = (..count..)/sum(..count..)), binwidth = 25) + 
   #    scale_y_continuous(labels = percent_format())
   
+  breaks <- pretty(range(df$var), n = nclass.FD(df$var), min.n = 1)
+  bwidth <- breaks[2] - breaks[1]
+  
   g <- ggplot(df, aes(x=var, fill=var)) +
-    geom_density(aes(y=..count..), binwidth=1, position="identity")
+    geom_density(aes(y=..count..), 
+                 binwidth=bwidth, position="identity")
   
   #  g <- ggplot(df, aes(var, ..density.., colour = category)) +
   #    scale_fill_continuous("Number of\nprojects") + 
@@ -189,7 +347,12 @@ plotDensity <- function (df, colName) {
   
   #  g <- g + geom_freqpoly(binwidth = 1)
   
-  print(g)
+  if (.Platform$GUI == "RStudio") {print(g)}
+  
+  #TODO: consider moving to main
+  edaFile <- str_replace_all(string=colName, pattern=" ", repl="")
+  edaFile <- file.path(EDA_RESULTS_DIR, paste0(edaFile, ".svg"))
+  suppressMessages(ggsave(file=edaFile, plot=g, width=8.5, height=11))
   
   return (g)
 }
@@ -201,6 +364,7 @@ plotBarGraph <- function (df, colName) {
   SHOW_LEVELS <- 10
   df <- df
   df$var <- factor(df[[colName]])
+  df <- na.omit(df)
   
   # sort factor levels by the frequency of levels
   df$var <- reorder(df$var, df$var, function(x) -length(x))
@@ -208,14 +372,36 @@ plotBarGraph <- function (df, colName) {
   
   title <- paste("Projects distribution across", colName, "range")
   
-  g <- ggplot(data=df, aes(x=var, fill=var)) +
+  # prepare to place percentage on top of bars, using geom_text()
+  if (FALSE) {
+    dfTab <- as.data.frame(table(df))
+    colnames(dfTab)[1] <- "x"
+    dfTab$lab <- as.character(100 * dfTab$Freq / sum(dfTab$Freq))
+  }
+  
+  # df[!is.na(df$var), ]
+  g <- ggplot(df, aes(x=var, fill=var)) +
     geom_bar(stat="bin", position="identity") +
     scale_fill_discrete(colName) + 
     xlab(colName) +
     ylab("Number of projects") +
     ggtitle(label=title)
   
-  print(g)
+  # display pre-calculated percentage on top of bars
+  if (FALSE) {
+    g <- g +
+      geom_text(data=dfTab,aes(x=x,y=Freq,label=lab),vjust=0) +
+      theme(axis.text.x=element_blank(),axis.ticks=element_blank(),
+            axis.title.x=element_blank(),legend.title=element_blank(),
+            axis.title.y=element_blank())  
+  }
+  
+  if (.Platform$GUI == "RStudio") {print(g)}
+  
+  #TODO: consider moving to main
+  edaFile <- str_replace_all(string=colName, pattern=" ", repl="")
+  edaFile <- file.path(EDA_RESULTS_DIR, paste0(edaFile, ".svg"))
+  suppressMessages(ggsave(file=edaFile, plot=g, width=8.5, height=11))
   
   return (g)
 }
@@ -237,7 +423,8 @@ ggQQplot <- function (vec, varName) # argument: vector of numbers
   g <- ggplot(d, aes(sample = resids)) +
     
     # Normal distribution function by default
-    stat_qq() +
+    # suppress "Removed N rows containing missing values (stat_qq)"
+    suppressWarnings(stat_qq()) +
     
     # Gamma distribution function
     #stat_qq(distribution = qgamma, dparams=list(shape=1)) +
@@ -250,7 +437,12 @@ ggQQplot <- function (vec, varName) # argument: vector of numbers
     scale_y_continuous("Sample Quantiles") +
     ggtitle(label=title)
   
-  print(g)
+  if (.Platform$GUI == "RStudio") {print(g)}
+  
+  #TODO: consider moving to main
+  edaFile <- str_replace_all(string=varName, pattern=" ", repl="")
+  edaFile <- file.path(EDA_RESULTS_DIR, paste0("QQ-", edaFile, ".svg"))
+  suppressMessages(ggsave(file=edaFile, plot=g, width=8.5, height=11))
   
   return (g)
 }
@@ -259,6 +451,13 @@ ggQQplot <- function (vec, varName) # argument: vector of numbers
 
 
 message("\n===== Starting Exploratory Data Analysis (EDA)...")
+
+fileName <- paste0(READY4EDA_FILE, RDS_EXT)
+ready4edaFile <- file.path(READY4EDA_DIR, fileName)
+
+# load data
+message("\nLoading data...")
+flossData <- loadData(ready4edaFile)
 
 # construct list of indicators & corresponding extra functions
 sfIndicators <- c("prjAge", "devTeamSize",
@@ -274,15 +473,16 @@ silent <- lapply(seq_along(sfIndicators), function(i) {
              sfIndicators[[i]], sfColumnNames[[i]], sfExtraFun[[i]])
 })
 
+
+edaFilePDF <- file.path(EDA_RESULTS_DIR, "eda-univar.pdf")
 mg <- do.call(marrangeGrob, c(allPlots, list(nrow=2, ncol = 1)));
-print(mg)
+suppressMessages(ggsave(filename=edaFilePDF, mg, width=8.5, height=11))
 
 message("\n===== EDA completed, results can be found ",
         "in directory \"", EDA_RESULTS_DIR, "\"\n")
 
 ## @knitr DoNotUse
-
-stop()
+stop('OK! Intentionally stopped to prevent code from running.')
 
 # construct list of indicators & corresponding extra functions
 sfMultiIndicators <- c("prjAge", "prjLicense")
@@ -298,7 +498,6 @@ multiPlots <- lapply(seq_along(sfMultiIndicators), function(i) {
 #edaFilePDF <- file.path(EDA_RESULTS_DIR, "eda-multivar.pdf")
 #pdf(edaFilePDF)
 #silent <- lapply(multiPlots, print)
-#dev.off()
 
 
 ##### "EXTRA" (CUSTOMIZATION) FUNCTIONS #####
