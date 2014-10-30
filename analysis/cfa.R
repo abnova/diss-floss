6b08a09f5b5f91a3b6d1b05e291d617a87d9b6b9
Aleksandr Blekh <blekh@nova.edu>
2014-10-29 23:07:28 -1100

    Implemented CFA for the pilot dataset and successfully tested it.

--- analysis/cfa.R
+++ analysis/cfa.R
@@ -1,42 +1,135 @@
 # Start session with a clean R environment
+## @knitr CleanEnv
 rm(list = ls(all.names = TRUE))
 
-if (!suppressMessages(require(psych))) install.packages('psych')
-if (!suppressMessages(require(mvtnorm))) install.packages('mvtnorm')
-#if (!suppressMessages(require(GPArotation))) 
-#  install.packages('GPArotation')
+##### PACKAGES #####
 
-library(psych)
-library(mvtnorm)
-#library(GPArotation)
+## @knitr LoadPackages
+if (!suppressMessages(require(polycor))) install.packages('polycor')
+if (!suppressMessages(require(lavaan))) install.packages('lavaan')
 
-PRJ_HOME <- Sys.getenv("DISS_FLOSS_HOME") # getwd()
+library(polycor)
+library(lavaan)
 
-# load data (commented out, as data are loaded in "missing.R")
-#source("../prepare/merge.R")
 
-# handle missing values (temporarily here)
-source(file.path(PRJ_HOME, "prepare/missing.R"))
+##### SETUP #####
+
+## @knitr PrepareCFA
+set.seed(100)
+
+PRJ_HOME <- Sys.getenv("DISS_FLOSS_HOME")
+
+KNITR <<- isTRUE(getOption("knitr.in.progress"))
+
+source(file.path(PRJ_HOME, "utils/data.R"))
+source(file.path(PRJ_HOME, "utils/platform.R"))
+
+READY4CFA_DIR  <- file.path(PRJ_HOME, "data/ready4cfa")
+READY4CFA_FILE <- "flossData" # default
+
+CFA_RESULTS_DIR <- file.path(PRJ_HOME, "results/cfa")
+
+RDS_EXT      <- ".rds"
+GRAPHICS_EXT <- ".svg"
+
+DEBUG <- TRUE
+
+
+##### ANALYSIS #####
 
 message("\n\n===== PERFORMING CONFIRMATORY FACTOR ANALYSIS (CFA) =====")
 
-message("\n\n*** Determining correlations between items...\n")
+fileName <- paste0(READY4CFA_FILE, RDS_EXT)
+ready4cfaFile <- file.path(READY4CFA_DIR, fileName)
 
-# estimate tetrachoric correlations between items
-message("\nTetrachoric correlations:")
-message("=========================\n")
+# load data
+message("\n\n*** Loading data...")
+flossData <- loadData(ready4cfaFile)
 
-message("Currently disabled.\n")
-#tetrachoric(flossData)
+# due to very small amount of projects with "Non-OSI" licesnse
+# and their disapperance due to calculating correlations,
+# we don't include "License Category" into EFA (consider analyzing it
+# at later phases with inclusion of imputed data)
 
-# estimate tetrachoric correlations between items
-message("\nPolychoric correlations:")
-message("========================\n")
+# we also remove "Repo URL" due to injection of large # of NAs
+# due to limiting conditionsat the end of the merge process
 
-# "You have more than 8 categories for your items,
-#  polychoric is probably not needed"
-message("Currently disabled.\n")
-#polychoric(flossData)
+factors4Analysis <- c("Development Team Size", "Project Age",
+                      "License Restrictiveness", "Project Stage",
+                      "Software Type")
+flossData <- flossData[factors4Analysis]
 
-#biserial(x, y)
-#polyserial(x, y)
+# convert names (temp)
+names(flossData) <- make.names(names(flossData))
+
+# sample the sample (use 1%) to reduce processing time
+#flossData <- sampleDF(flossData, nrow(flossData) / 100)
+
+# save name of the data set (seems redundant, but it will be useful,
+# when there will be more than one data set, i.e. 'pilot' and 'main')
+# [currently used for KNITR only]
+datasetName <- deparse(substitute(flossData))
+
+
+# ===
+
+if (FALSE) {
+  message("\nCovariance matrix:")
+  message("-------------------")
+  cov.info <- cov(flossData, use = "pairwise.complete.obs")
+  print(cov.info, digits = 2)
+}
+
+message("\n*** Calculating correlations...")
+corr.info <- hetcor(flossData, use="pairwise.complete.obs",
+                    std.err = TRUE)
+
+if (DEBUG) {
+  message("\nCorrelations matrix:")
+  message("--------------------")
+  print(corr.info, digits = 2)
+}
+
+# calculate standard deviation for each variable in the data set
+sdValues <- unlist(lapply(seq(length(names(flossData))),
+                          function(i) sd(flossData[[i]], na.rm = TRUE)))
+
+message("\nCovariance matrix:")
+message("-------------------")
+cov.info <- cor2cov(corr.info$correlations, sdValues)
+print(cov.info, digits = 2)
+
+
+# specify the latent variable model
+
+model <- "
+
+# factor structure
+
+f1 =~ 1 * Development.Team.Size
+f2 =~ 1 * License.Restrictiveness
+f3 =~ 1 * Project.Age + Software.Type
+
+# fix some variances/residual variances to zero
+# because < 3 indicators per latent variables
+
+Development.Team.Size ~~ 0 * Development.Team.Size
+License.Restrictiveness ~~ 0 * License.Restrictiveness
+Project.Age ~~ 0 * Project.Age
+Software.Type ~~ 0 * Software.Type
+
+# covariances between the latent variables
+
+f1 ~~ f2
+f1 ~~ f3
+f2 ~~ f3
+"
+
+# perform CFA
+message("\n*** Performing CFA of the model...")
+
+fit <- cfa(model, data = flossData, meanstructure = TRUE,
+           missing = "pairwise", estimator = "WLSMV")
+
+# output results
+summary(fit, fit.measures = TRUE, standardize = TRUE)
