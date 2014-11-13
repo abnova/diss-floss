bd37a18db8119c3652d1ada6692c263d08c50659
Aleksandr Blekh <blekh@nova.edu>
2014-11-12 23:24:13 -1100

    Implemented generic function for generating dynamic [knitr] chunks with automatic cross-referencing of objects (figures/tables) within a document (currently supports figures only).

--- utils/knit.R
+++ utils/knit.R
@@ -1,19 +1,29 @@
 # Dynamically generate knit-ready chunk code for objects (figures/tables),
 # ready for cross-referencing in a report document (supports .Rmd to LaTeX).
 
-genDynChunk <- function (obj, multi = FALSE, hold = TRUE) {
+# Currently supports figures only,
+# as tables generation is context- and target-specific (TBD).
+
+genDynChunks <- function (refInfo, multi = FALSE, hold = TRUE) {
   
-  latexObjLabel <- paste0("{{name}}\\\\label{", bcRefStr$objType, ":{{name}}", "}")
+  latexObjLabel <- paste0("{{caption}}", "\\\\label{", refInfo$objType, ":{{varName}}", "}")
   
-  chunkName <- "{{name}}"
-  chunkHeader <- paste0("```{r ", chunkName, ", ")
-  chunkOptions <- paste0("include=TRUE, results='asis', fig.height=4, fig.width=4, fig.cap='", latexObjLabel, "'")
-  chunkHeaderFull <- paste0(chunkHeader, chunkOptions, "}")
+  chunkName <- "{{name2}}"
+  chunkHeader <- paste0("```{r ", chunkName, ",")
+  chunkOptions <- paste0("include=TRUE, results='asis', fig.height=3.5, fig.width=7, fig.cap='", latexObjLabel, "'")
+  chunkHeaderFull <- paste(chunkHeader, chunkOptions, "}")
   chunkBody <- "print(get('{{name}}'))"
   
   chunkText <- c(chunkHeaderFull,
                  chunkBody,
                  "```", "\n")
   
-  figReportParts <- lapply(bcRefStr$objs, function (x) knit_expand(text = chunkText, name = x))
+  objChunks <- lapply(refInfo$objs, function (x) 
+    knit_expand(text = chunkText,
+                name = x,
+                name2 = gsub('\\.', '_', x),
+                varName = strsplit(x, refInfo$objTypePrefix)[[1]][2],
+                caption = attr(get(x), 'title')))
+  
+  return (unlist(objChunks))
 }
