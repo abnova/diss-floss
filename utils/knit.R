09c03334a3f265e584554c15f2b066bc712af795
Aleksandr Blekh <blekh@nova.edu>
2014-11-18 13:06:14 -1100

    Extracted 'knitr'-related functions from EDA code and moved them to the corresponding module for project-wide re-use. Now "interested" modules have to source "utils/knitr.R" prior to use of these functions.

--- utils/knit.R
+++ utils/knit.R
@@ -34,3 +34,76 @@ genDynChunks <- function (refInfo, multi = FALSE, hold = TRUE,
   
   return (unlist(objChunks))
 }
+
+
+genEDAdescStatsTable <- function (df, label = "edaDescStats",
+                                  caption = "EDA descriptive statistics",
+                                  digits = 2) {
+  
+  df <- df[, sapply(df, is.numeric)]
+  df <- psych::describe(df)
+  
+  df <- as.data.frame(round(df, digits))
+  
+  df$vars <- rownames(df)
+  colsToInclude <- c("n", "mean", "sd", "median",
+                     "min", "max", "skew", "kurtosis")
+  tableCols <- c("N", "Mean", "SD", "Median",
+                 "Min", "Max", "Skew", "Kurtosis")
+  
+  df <- df[-1, ]  # remove Project.ID
+  df <- df[, colsToInclude]
+  names(df) <- tableCols
+  
+  edaDescStatsTable <- as.tabular(df)
+  
+  # set the caption (specific for 'tables' package)
+  latexCap <- paste0("\\caption{", caption, ".}\\\\", "\n",
+                     "\\toprule",
+                     "\\label{tab:", label, "}")
+  
+  # set tabular settings
+  booktabs()
+  
+  # output LaTeX table
+  latex(edaDescStatsTable,
+        mathmode = FALSE, # output dash instead of LaTeX minus sign character
+        options = list(tabular = "longtable",
+                       toprule = latexCap))
+}
+
+
+# produces string with LaTeX reference labels for figures/tables of specific type
+genObjRefs <- function (objType, objTypePrefix) {
+  
+  objs <- ls(pattern = objTypePrefix, envir = .GlobalEnv)
+  if (length(objs) == 0)
+    stop(paste("No objects of type", objTypePrefix, "found!"))
+  
+  split <- strsplit(objs, objTypePrefix)
+  objRefs <- sapply(split, `[[`, 2)
+  #objRefs <- split[[seq(split)]][2]
+  
+  objAllRefs <- c()
+  for (i in seq(objRefs)) objAllRefs <- c(objAllRefs, objRefs[[i]])
+  refKeyword <- ifelse(objType == "fig", "\\ref{fig:", "\\ref{tab:")
+  refStr <- sapply(objAllRefs, function (x) {paste0(refKeyword, x, "}")})
+  
+  colFlag <- ""; refStrTemp <- ""
+  objWord <- ifelse(objType == "fig", "Figures ", "Tables ")
+  
+  if (length(refStr) < 2) {
+    objWord <- ifelse(objType == "fig", "Figure ", "Table")
+    refStrFinal <- paste(objWord, refStr[length(refStr)])
+  }
+  else {
+    if (length(refStr) == 2) colFlag <- " and "
+    else if (length(refStr) > 2) colFlag <- ", "
+    
+    refStrTemp <- paste(refStr[-length(refStr)], collapse = colFlag)
+    refStrFinal <- paste(objWord, refStrTemp, " and ", refStr[length(refStr)])
+  }
+  
+  list(objs = objs, str = refStrFinal,
+       objType = objType, objTypePrefix = objTypePrefix)
+}
